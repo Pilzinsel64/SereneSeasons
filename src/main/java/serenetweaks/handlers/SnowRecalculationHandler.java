94f3511b32cc580da8e2e0171470f0afc79de0c9
Fabian Maurer <dark.shadow4@web.de>
2023-05-14 23:20:53 +0200

    Format

--- src/main/java/serenetweaks/handlers/SnowRecalculationHandler.java
+++ src/main/java/serenetweaks/handlers/SnowRecalculationHandler.java
@@ -22,9 +22,9 @@ import sereneseasons.season.SeasonASMHelper;
 import serenetweaks.data.TimeStampsWorldSavedData;
 
 public class SnowRecalculationHandler {
-	
+
 	private static ArrayList<Chunk> recalculationQueue = new ArrayList<Chunk>();
-	
+
 	@SubscribeEvent
 	public void onTick(TickEvent.WorldTickEvent event) {
 		Type type = event.type;
@@ -50,30 +50,30 @@ public class SnowRecalculationHandler {
 			if (chunk.isChunkLoaded && chunk.isTerrainPopulated) {
 				boolean success = true;
 				for (int k2 = 0; k2 < 16; ++k2) {
-		            for (int j3 = 0; j3 < 16; ++j3) {
-		                int posX = chunk.xPosition * 16 + k2;
-		                int posZ = chunk.zPosition * 16 + j3;
-		                int posY1 = chunk.getPrecipitationHeight(k2, j3);
-		                int posY2 = posY1 - 1;
-	
-		                if (world.canBlockFreeze(posX, posY2, posZ, false)) {
-		                	success = success && world.setBlock(posX, posY2, posZ, Blocks.ice, 0, 2);
-		                }
-	
-		                if (world.canSnowAtBody(posX, posY1, posZ, true)) {
-		                	success = success && world.setBlock(posX, posY1, posZ, Blocks.snow_layer, 0, 2);
-		                }
-		                
-		                if (shouldMelt(world, posX, posY1, posZ)) {
-		                	if (world.getBlock(posX, posY2, posZ) == Blocks.ice) {
-		                		success = success && world.setBlock(posX, posY2, posZ, Blocks.water, 0, 2);
-		                	}
-		                	if (world.getBlock(posX, posY1, posZ) == Blocks.snow_layer) {
-		                		success = success && world.setBlock(posX, posY1, posZ, Blocks.air, 0, 2);
-		                	}
-		                }
-		            }
-		        }
+					for (int j3 = 0; j3 < 16; ++j3) {
+						int posX = chunk.xPosition * 16 + k2;
+						int posZ = chunk.zPosition * 16 + j3;
+						int posY1 = chunk.getPrecipitationHeight(k2, j3);
+						int posY2 = posY1 - 1;
+
+						if (world.canBlockFreeze(posX, posY2, posZ, false)) {
+							success = success && world.setBlock(posX, posY2, posZ, Blocks.ice, 0, 2);
+						}
+
+						if (world.canSnowAtBody(posX, posY1, posZ, true)) {
+							success = success && world.setBlock(posX, posY1, posZ, Blocks.snow_layer, 0, 2);
+						}
+
+						if (shouldMelt(world, posX, posY1, posZ)) {
+							if (world.getBlock(posX, posY2, posZ) == Blocks.ice) {
+								success = success && world.setBlock(posX, posY2, posZ, Blocks.water, 0, 2);
+							}
+							if (world.getBlock(posX, posY1, posZ) == Blocks.snow_layer) {
+								success = success && world.setBlock(posX, posY1, posZ, Blocks.air, 0, 2);
+							}
+						}
+					}
+				}
 				if (success) {
 					recalculationQueue.remove(i);
 					c++;
@@ -84,12 +84,12 @@ public class SnowRecalculationHandler {
 				i++;
 			}
 		}
-		
+
 	}
-	
+
 	@SubscribeEvent
 	public void onChunkLoaded(ChunkEvent.Load event) {
-		if(!ModConfig.seasons.shouldRecalculateSnow) {
+		if (!ModConfig.seasons.shouldRecalculateSnow) {
 			return;
 		}
 		World world = event.world;
@@ -100,16 +100,16 @@ public class SnowRecalculationHandler {
 		if (world.provider.dimensionId != 0) {
 			return;
 		}
-		int currentTime = (int) (System.currentTimeMillis()/1000/60);
+		int currentTime = (int) (System.currentTimeMillis() / 1000 / 60);
 		int savedTime = TimeStampsWorldSavedData.getChunkTimeStamp(chunk);
 		if (currentTime - savedTime > ModConfig.seasons.timeToRecalculateSnow) {
 			recalculationQueue.add(chunk);
 		}
 	}
-	
+
 	@SubscribeEvent
 	public void playerJoinedWorld(PlayerLoggedInEvent event) {
-		if(!ModConfig.seasons.shouldRecalculateSnow) {
+		if (!ModConfig.seasons.shouldRecalculateSnow) {
 			return;
 		}
 		EntityPlayer player = event.player;
@@ -122,8 +122,8 @@ public class SnowRecalculationHandler {
 		}
 		for (int i = -5; i < 5; i++) {
 			for (int j = -5; j < 5; j++) {
-				Chunk chunk = world.getChunkFromChunkCoords(((int)player.posX/16) + i, ((int)player.posZ/16) + j);
-				int currentTime = (int) (System.currentTimeMillis()/1000/60);
+				Chunk chunk = world.getChunkFromChunkCoords(((int) player.posX / 16) + i, ((int) player.posZ / 16) + j);
+				int currentTime = (int) (System.currentTimeMillis() / 1000 / 60);
 				int savedTime = TimeStampsWorldSavedData.getChunkTimeStamp(chunk);
 				if (currentTime - savedTime > ModConfig.seasons.timeToRecalculateSnow) {
 					recalculationQueue.add(chunk);
@@ -131,7 +131,7 @@ public class SnowRecalculationHandler {
 			}
 		}
 	}
-	
+
 	@SubscribeEvent
 	public void onChunkUnLoaded(ChunkEvent.Unload event) {
 		World world = event.world;
@@ -142,12 +142,12 @@ public class SnowRecalculationHandler {
 		if (world.provider.dimensionId != 0) {
 			return;
 		}
-		if(!removeFromRecalculationQueue(chunk)) {
-			int currentTime = (int) (System.currentTimeMillis()/1000/60);
+		if (!removeFromRecalculationQueue(chunk)) {
+			int currentTime = (int) (System.currentTimeMillis() / 1000 / 60);
 			TimeStampsWorldSavedData.setChunkTimeStamp(chunk, currentTime);
 		}
 	}
-	
+
 	@SubscribeEvent
 	public void playerLeftWorld(PlayerLoggedOutEvent event) {
 		EntityPlayer player = event.player;
@@ -158,29 +158,28 @@ public class SnowRecalculationHandler {
 		if (world.isRemote) {
 			return;
 		}
-		int currentTime = (int) (System.currentTimeMillis()/1000/60);		
+		int currentTime = (int) (System.currentTimeMillis() / 1000 / 60);
 		for (int i = -5; i < 5; i++) {
 			for (int j = -5; j < 5; j++) {
-				Chunk chunk = world.getChunkFromChunkCoords(((int)player.posX/16) + i, ((int)player.posZ/16) + j);
-				if(!removeFromRecalculationQueue(chunk)) {
+				Chunk chunk = world.getChunkFromChunkCoords(((int) player.posX / 16) + i, ((int) player.posZ / 16) + j);
+				if (!removeFromRecalculationQueue(chunk)) {
 					TimeStampsWorldSavedData.setChunkTimeStamp(chunk, currentTime);
 				}
 			}
 		}
 	}
-	
+
 	private boolean shouldMelt(World world, int x, int y, int z) {
 		BiomeGenBase biome = world.getBiomeGenForCoords(x, z);
-        SubSeason subSeason = SeasonHelper.getSeasonState(world).getSubSeason();
-        float f = SeasonASMHelper.getFloatTemperature(subSeason, biome, x, y, z);
-        
-		if (f >= 0.15F)
-        {
-            return true;
-        }
+		SubSeason subSeason = SeasonHelper.getSeasonState(world).getSubSeason();
+		float f = SeasonASMHelper.getFloatTemperature(subSeason, biome, x, y, z);
+
+		if (f >= 0.15F) {
+			return true;
+		}
 		return false;
 	}
-	
+
 	private boolean removeFromRecalculationQueue(Chunk chunk) {
 		for (int i = 0; i < recalculationQueue.size(); i++) {
 			Chunk queueChunk = recalculationQueue.get(i);
@@ -192,5 +191,5 @@ public class SnowRecalculationHandler {
 		}
 		return false;
 	};
-	
+
 }
